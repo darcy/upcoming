c585024b9980e391834a1a7c67f7119d93afbfd2
Darcy Brown <darcy@dbcodeproject.com>
2010-05-24 00:35:37 -0500

    added support for a prefix

--- lib/upcoming.rb
+++ lib/upcoming.rb
@@ -5,8 +5,9 @@ module Upcoming
     def upcoming title=nil, options={}, &block
       return unless UpcomingConfig.enabled?
       initialize_upcoming unless @upcoming_initialized
-      title = title.nil? ? "Upcoming" : title
-      concat(render(:partial => "#{UpcomingConfig.view_dir}/upcoming", :locals => {:options=>options, :body => capture(&block), :title => title}), block.binding)
+      title = title.nil? ? "Upcoming" : title.to_s
+      title = UpcomingConfig.prefix + title if UpcomingConfig.prefix
+      concat(render(:partial => "#{UpcomingConfig.view_dir}/upcoming", :locals => {:options=>options, :body => capture(&block), :title => title}))
     end
 
     def initialize_upcoming
@@ -18,12 +19,14 @@ module Upcoming
   end
 
   class UpcomingConfig
+    @prefix = nil
     @view_dir = "upcoming"
     @environments = ['development']
     
     class << self 
       attr_accessor :environments
       attr_accessor :view_dir
+      attr_accessor :prefix
     end
   
     def self.enabled?
